3dbd9be95689f053458b8f9a6f5839cca30a52d6
Ryam Sousa <ryam.sousa123@gmail.com>
2021-12-29 17:38:46 -0300

    footer behavior

--- ui.frontend/src/app/shared/footer/footer.component.ts
+++ ui.frontend/src/app/shared/footer/footer.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  author: string;
+  email: string;
   editMode: boolean;
 
   constructor() {
